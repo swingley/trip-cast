16299b2b73f59f243295399e465d13a389dc869b
swingley <swingley@gmail.com>
2017-09-01 16:11:34 -0700

    Tweak spinner position.

--- src/spinner.js
+++ src/spinner.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Spinner extends Component {
   render () {
     return (
-      <svg width="50" height="50" className="App-spin">
+      <svg width="50" height="50" className="loading">
         <line className="rotate" x1="20" y1="30" x2="30" y2="20" />
       </svg>
     )
